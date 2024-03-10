e3e55cc59fae4f6e29fbaac841024ee467999329
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2024-03-10 20:19:14 -0300

    coment the components that was empty bc broke my code otherwise

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import "./App.css";
 import NavBar from "./assets/components/NavBar";
 import Form from "./assets/components/Form";
-import TaskTypeSelector from "./assets/components/TaskTypeSelector";
-import ListOfTask from "./assets/components/ListOfTasks";
-import Task from "./assets/components/Task";
-import Footer from "./assets/components/Footer";
+// import ListOfTask from "./assets/components/ListOfTasks";
+// import Task from "./assets/components/Task";
+// import Footer from "./assets/components/Footer";
 
 function App() {
   return (
     <>
       <NavBar />
       <Form />
-      <TaskTypeSelector />
-      <ListOfTask />
-      <Task />
-      <Footer />
+      {/* <ListOfTask />
+      <Task /> */}
+      {/* <Footer /> */}
     </>
   );
 }
