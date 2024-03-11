33c8bb7726a62ec724ef22b29d30e41552475100
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2024-03-10 23:39:43 -0300

    change the input for a component in materia ui and give style to organice the screen

--- src/assets/components/Form.jsx
+++ src/assets/components/Form.jsx
@@ -1,9 +1,13 @@
-import TaskTypeSelector from  './TaskTypeSelector'
 import { useState } from "react";
 
-const Form = ({lsTask, setLsTask}) => {
+import TaskTypeSelector from  './TaskTypeSelector'
 
-  const [loadedTask, setLoadedTask] = useState("");//VA ENVIANDO Y TRAYENDO LO QUE ESTUVO ESCRIBIENDO EN EL INPUT ?????
+import Box from "@mui/material/Box"; //le da estilos a mi tag box
+import TextField from '@mui/material/TextField';
+
+
+const Form = ({ lsTask, setLsTask }) => {
+  const [loadedTask, setLoadedTask] = useState(""); //VA ENVIANDO Y TRAYENDO LO QUE ESTUVO ESCRIBIENDO EN EL INPUT ?????
 
   const getTaskAdded = (e) => {
     setLoadedTask(e.target.value);
@@ -14,29 +18,29 @@ const Form = ({lsTask, setLsTask}) => {
     handleAddTask(loadedTask);
   }; //SE INICIA AL CLICK EL BTN CARGAR Y SOLO EJECUTA HANDLEADDTASk ENVIANDOLE LO QUE SE ESCRIBIO EN EL INPUT~
 
-
-
-
-
-  const handleAddTask = (loadedTask) => { //LE LLEGA LO QUE SE PUSO EN EL INPUT
+  const handleAddTask = (loadedTask) => {
+    //LE LLEGA LO QUE SE PUSO EN EL INPUT
     const valor = { id: crypto.randomUUID(), task: loadedTask }; //SE GUARDA EN VALOR LO QUE SE ESCRIBIO EN EL INPUT Y SE LE DA UN ID
     setLsTask([...lsTask, valor]); //SE AGREGA EL NUEVO OBJETO CREADO
     localStorage.setItem("taskName", JSON.stringify([...lsTask, valor])); //SE AGREGA AL LS EL NUEVO OBJETO CREADO
   };
 
-
-  
   return (
-    <form onSubmit={(e) => manejarClick(e)}>
-      <div>
-        <div>
-          <label htmlFor="taskNameInput">Tarea</label>
-          <input type="text" id="taskNameInput" name="taskName" onChange={getTaskAdded} />
-        </div>
+      <Box
+        component="form"
+        sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: 2,
+          }}
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => manejarClick(e)}
+      >
+        <TextField id="outlined-basic" label="Task" variant="outlined" onChange={getTaskAdded}/>{" "}
         <TaskTypeSelector />
-      </div>
-      <button type="submit">Cargar</button>
-    </form>
+        <button type="submit" >Cargar</button>
+      </Box>
   );
 };
 export default Form;
