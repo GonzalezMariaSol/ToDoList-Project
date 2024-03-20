1013a646f2b78bd5e0e841f9aef89f1749ede6cd
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2024-03-20 13:34:02 -0300

    added the validation of quantity of characters for a task and clean the input after enter

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,15 +6,24 @@ import { Box, TextField } from '@mui/material';
 
 const Form = ({ lsTask, setLsTask }) => {
   const [loadedTask, setLoadedTask] = useState(""); //VA ENVIANDO Y TRAYENDO LO QUE ESTUVO ESCRIBIENDO EN EL INPUT ?????
+  const [error, setError] = useState(''); //va a estar corroborando que mi form se cumpla con lo pedido
 
   const getTaskAdded = (e) => {
-    setLoadedTask(e.target.value);
+    setLoadedTask(e.target.value)
   }; //SOLO CAPTURA LO QUE ESCRIBE EL USUARIO EN EL INPUT
 
   const manageClick = (e) => {
-    if (e.key === "Enter") {//permite cargar el task sin tener que hacer click en cargar
+     if (e.target.value.length < 5) {
+      setError('¡Debes escribir al menos 5 caracteres!');
+    } else if (e.target.value.length > 40) {
+      setError('¡Debes escribir menos de 40 caracteres!');
+    } else {
+      setError('');
+    }
+    if (e.key === 'Enter' && e.target.value.length > 5 && e.target.value.length <= 40) {//permite cargar el task sin tener que hacer click en cargar
       e.preventDefault(); //evita que se auto refresque antes de lo debido
       saveAddedTask(loadedTask); //inicializamos saveAddedTask con el valor que se escribio en el text field
+      e.target.value = ''; //limpio el input luego de haber hecho enter
       //! COMO PUEDO LIMPIAR EL TEXTFIELD LUEGO DEL ENTER?
     }
   };
@@ -48,6 +57,8 @@ const Form = ({ lsTask, setLsTask }) => {
         }}
         onChange={getTaskAdded}
         onKeyDown={(e) => manageClick(e)}
+        error={!!error}//es una prop de mui cuando error sea exactamente true (q SI hay error) 
+        helperText={error}//es una prop de mui y si error es true, entonces muestre el texto de ayuda (que se guarda en error)
       />
       <TaskTypeSelector lsTask={lsTask} setLsTask={setLsTask}/>
       {/* <button type="submit" >Cargar</button> */}
